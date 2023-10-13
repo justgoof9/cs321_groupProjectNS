af3b38b3bdf7ba39ff264d9badba7717830ba7a0
ssalem27 <safialhasi@gmail.com>
2023-10-13 15:28:25 -0400

    testing for citizen

--- Citizen.java
+++ Citizen.java
@@ -1,3 +1,47 @@
 public class Citizen {
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String ssn;
+    private String email;
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
     
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
 }
--- TestCitizen.java
+++ TestCitizen.java
@@ -12,7 +12,36 @@ public class TestCitizen {
         citizen.setFirstName(name);
         assertThat(citizen.getFirstName(), is(name));
     }
-    
 
-    
+    @Test
+    public void testGetSetLastName(){
+        Citizen citizen = new Citizen();
+        String name = "testName";
+        citizen.setLastName(name);
+        assertThat(citizen.getLastName(), is(name));
+    }
+
+    @Test
+    public void testGetSetDob(){
+        Citizen citizen = new Citizen();
+        String name = "testName";
+        citizen.setDob(name);
+        assertThat(citizen.getDob(), is(name));
+    }
+
+    @Test
+    public void testGetSetSsn(){
+        Citizen citizen = new Citizen();
+        String name = "testName";
+        citizen.setSsn(name);
+        assertThat(citizen.getSsn(), is(name));
+    }
+
+    @Test
+    public void testGetSetEmail(){
+        Citizen citizen = new Citizen();
+        String name = "testName";
+        citizen.setEmail(name);
+        assertThat(citizen.getEmail(), is(name));
+    }
 }
