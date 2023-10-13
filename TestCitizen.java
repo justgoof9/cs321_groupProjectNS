d4dc9b0ee1eafb28d4f28adf94ed8a2adc7d6ef8
ssalem27 <safialhasi@gmail.com>
2023-10-13 14:41:50 -0400

    add testCitizen file

---
+++ TestCitizen.java
@@ -0,0 +1,13 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class TestCitizen {
+    //test Citizen class
+    @Test
+    public void testCGetFirstName(){
+        Citizen citizen = new Citizen();
+
+    }
+}
