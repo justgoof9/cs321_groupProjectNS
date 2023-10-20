6b1be680640983aa9f2c6e5767f23bcb510af96b
Nischal Shrestha <justgoof9@gmail.com>
2023-10-19 22:14:04 -0400

    Finished test cases & comments for TestWorkflow

--- TestWorkflow.java
+++ TestWorkflow.java
@@ -9,19 +9,50 @@ import java.util.UUID;
 
 
 public class TestWorkflow {
+    /**
+     * Test the setting and retreival of the approval queue in the Workflow class.
+     */
     @Test
     public void testGetSetApprovalQueue() {
         Workflow wf = new Workflow();
-        LinkedList<UUID> ap = new LinkedList<>(null);
+        LinkedList<UUID> ap = new LinkedList<>();
         wf.setApprovalQueue(ap);
-        asserThat(wf.getApprovalQueue(), is(ap));
+        assertThat(wf.getApprovalQueue(), is(ap));
     }
+    /**
+     * Test the setting and retrevial of the review queue in the Workflow class.
+     */
     @Test
     public void testGetSetReviewQueue() {
         Workflow wf = new Workflow();
-        LinkedList<UUID> rq = new LinkedList<>(null);
+        LinkedList<UUID> rq = new LinkedList<>();
         wf.setReviewQueue(rq);
         assertThat(wf.getReviewQueue(), is(rq));
     }
+    /**
+     * Test the addition of review to the review queue in the workflow class.
+     */
+    @Test
+    public void testAddReview() {
+        UUID id = new UUID(0,0);
+        Workflow wf = new Workflow();
+        LinkedList<UUID> rq = new LinkedList<>();
+        rq.push(id);
+        wf.addReview(id);
+        assertThat(wf.getReviewQueue(), is(rq));
+    }
+    /**
+     * Test the addition of an approval to the approval queue in the Workflow class.
+     */
+    @Test
+    public void testAddApproval() {
+        UUID id = new UUID(0,0);
+        Workflow wf = new Workflow();
+        LinkedList<UUID> rq = new LinkedList<>();
+        rq.push(id);
+        wf.addApproval(id);
+        assertThat(wf.getApprovalQueue(), is(rq));
+    }
+
     
 }
