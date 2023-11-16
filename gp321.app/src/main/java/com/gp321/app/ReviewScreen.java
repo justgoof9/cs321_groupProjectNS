7f5dab222db36e18d06feba24f66b6b5023a0198
ssalem27 <safialhasi@gmail.com>
2023-11-16 15:32:30 -0500

    review screen

--- gp321.app/src/main/java/com/gp321/app/ReviewScreen.java
+++ gp321.app/src/main/java/com/gp321/app/ReviewScreen.java
@@ -1,37 +1,127 @@
 package com.gp321.app;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ReviewScreen extends JFrame {
-
+    DataLayer dataLayer;
     public ReviewScreen() {
-        // Create a button with text "Click Me"
-        JButton button = new JButton("Click Me");
-        
+        //set properties
+        setTitle("USCIS Immigration Approval");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create a panel to hold the button
-        JPanel panel = new JPanel();
-        panel.add(button);
+        //create jpanel
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new GridLayout(10, 2, 10, 10));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        // Add the panel to the frame
-        this.add(panel);
+        //create labels for immigrant information
+        JLabel titleLabel = new JLabel("USCIS Immigration Review");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
 
-        // Set up the frame
-        this.setSize(300, 200); // Set the size of the frame
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the default close operation
-        this.setTitle("Simple GUI Application"); // Set the title of the frame
+        JLabel citizenLabel = new JLabel("Citizen Info");
+
+        JLabel nameLabel = new JLabel("Full Name:");
+        JTextArea nameValue = new JTextArea();
+
+        JLabel dobLabel = new JLabel("Date of Birth:");
+        JTextArea dobValue = new JTextArea();
+
+        JLabel ssnLabel = new JLabel("SSN:");
+        JTextArea ssnValue = new JTextArea();
+
+        JLabel emailLabel = new JLabel("Email:");
+        JTextArea emailValue = new JTextArea();
+
+        JLabel immigranLabel = new JLabel("NonImmigrant Info");
+
+        JLabel immNameLabel = new JLabel("Full Name:");
+        JTextArea immNameValue = new JTextArea();
+
+        JLabel immDobLabel = new JLabel("Date of Birth:");
+        JTextArea immDobValue = new JTextArea();
+
+        JLabel aNumLabel = new JLabel("Alien Number:");
+        JTextArea aNumValue = new JTextArea();
+
+        //add immigrant information to the panel
+        infoPanel.add(titleLabel);
+        infoPanel.add(new JLabel()); //spacing
+        infoPanel.add(citizenLabel);
+        infoPanel.add(new JLabel()); //spacing
+        infoPanel.add(nameLabel);
+        infoPanel.add(nameValue);
+        infoPanel.add(dobLabel);
+        infoPanel.add(dobValue);
+        infoPanel.add(ssnLabel);
+        infoPanel.add(ssnValue);
+        infoPanel.add(emailLabel);
+        infoPanel.add(emailValue);
+        infoPanel.add(immigranLabel);
+        infoPanel.add(new JLabel()); //spacing
+        infoPanel.add(immNameLabel);
+        infoPanel.add(immNameValue);
+        infoPanel.add(immDobLabel);
+        infoPanel.add(immDobValue);
+        infoPanel.add(aNumLabel);
+        infoPanel.add(aNumValue);
+
+        //add buttons
+        JButton approveButton = new JButton("Get Next Review");
+
+        JButton getNextReviewButton = new JButton("Reviewed");
+
+        JButton sendEmailButton = new JButton("Send Email");
+
+        //event handling
+        approveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(ReviewScreen.this, "Getting Next Review");
+            }
+        });
+
+        getNextReviewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(ReviewScreen.this, "Application Reviewed");
+            }
+        });
+
+        sendEmailButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(ReviewScreen.this, "Sending Email");
+            }
+        });
+
+        //create panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1, 2, 10, 10));
+        buttonPanel.add(approveButton);
+        buttonPanel.add(getNextReviewButton);
+        buttonPanel.add(sendEmailButton);
+
+        //create container for info and buttons
+        JPanel containerPanel = new JPanel();
+        containerPanel.setLayout(new BorderLayout());
+        containerPanel.add(infoPanel, BorderLayout.CENTER);
+        containerPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        //add container
+        add(containerPanel);
+
+        //center fram
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        // Schedule a job for the event dispatch thread: creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                // Create and display the GUI.
                 ReviewScreen frame = new ReviewScreen();
                 frame.setVisible(true);
             }
